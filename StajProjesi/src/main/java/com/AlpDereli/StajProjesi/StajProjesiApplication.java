cf075f57f7dcef640a76ac1ac4cf3860ce74f051
AlpDereli <alpdereli26@gmail.com>
2024-08-07 16:05:37 +0300

    Initial commit

---
+++ StajProjesi/src/main/java/com/AlpDereli/StajProjesi/StajProjesiApplication.java
@@ -0,0 +1,13 @@
+package com.AlpDereli.StajProjesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StajProjesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StajProjesiApplication.class, args);
+	}
+		system.out.println("hello world");
+}
---
+++ StajProjesi/src/test/java/com/AlpDereli/StajProjesi/StajProjesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AlpDereli.StajProjesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StajProjesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
