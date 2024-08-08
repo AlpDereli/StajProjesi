ffe363d233100c797c07ab5686c45cfddcc60543
AlpDereli <alpdereli26@gmail.com>
2024-08-08 19:46:32 +0300

    SecurityConfig.java

--- StajProjesi/src/main/java/com/AlpDereli/StajProjesi/config/SecurityConfig.java
+++ StajProjesi/src/main/java/com/AlpDereli/StajProjesi/config/SecurityConfig.java
@@ -1,13 +1,32 @@
 package com.AlpDereli.StajProjesi.config;
 
 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/organization/**").hasRole("ORGANIZATION")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll())
+                .logout(logout -> logout
+                        .permitAll());
+        return http.build();
     }
 
+}
